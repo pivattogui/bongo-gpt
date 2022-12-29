651d2c612211208b76b34dac997c7483dfc2fc65
Pivatto <55469735+pivattogui@users.noreply.github.com>
2022-12-29 00:48:08 -0300

    fix: remove my api key :v

--- src/lib/openai.ts
+++ src/lib/openai.ts
@@ -2,7 +2,7 @@ import { Configuration, OpenAIApi } from 'openai';
 
 //FIXME: This is a temporary API key. Please use your own API key.
 const configuration = new Configuration({
-    apiKey: "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",
+    apiKey: process.env.OPENAI_API_KEY,
 });
 
 const openai = new OpenAIApi(configuration);
