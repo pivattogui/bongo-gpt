8def3f5fa316180da422f971856653d4f8b95149
Pivatto <55469735+pivattogui@users.noreply.github.com>
2022-12-29 18:44:33 -0300

    styles

--- src/components/HeaderBongo.tsx
+++ src/components/HeaderBongo.tsx
@@ -2,7 +2,7 @@ import Image from "next/image"
 
 function HeaderBongo() {
     return (
-        <div className='flex flex-col items-center mb-4 text-white sm:mt-0 mt-16'>
+        <div className='flex flex-col items-center mb-4 text-white sm:mt-8 mt-16'>
             <Image width={300} height={300} src="/bongo-cat.gif" alt="Bongo Cat" />
             <span className="text-xs tracking-4 text-white/80">Totalmente não profissional</span>
             <span className=' font-bold text-3xl'>
